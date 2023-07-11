8e776f9928b96544fc428ed34fab7e39d4424393
Maxim <toper.one111@gmail.com>
2023-07-11 20:05:51 +0300

    Add user control functions

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 
 import userRoutes from './routes/userRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
 
@@ -12,6 +13,9 @@ const app = express();
 app.use('/api/users', userRoutes);
 
 app.get('/', (req, res) => res.send('server is ready'));
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`App is running on port ${port}`);
