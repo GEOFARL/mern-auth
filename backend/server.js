a0cbd361cb0c6eb0c4d2b6c762c3c202b8427481
Maxim <toper.one111@gmail.com>
2023-07-11 19:55:48 +0300

    Provide a basic setup

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,18 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
+import userRoutes from './routes/userRoutes.js';
+
 dotenv.config();
 
 const port = process.env.PORT || 5000;
 
 const app = express();
 
+app.use('/api/users', userRoutes);
+
 app.get('/', (req, res) => res.send('server is ready'));
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log(`App is running on port ${port}`);
 });
