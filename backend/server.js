1726373d4c65b5d1a6af78e7a630b029df70af3e
Maxim <toper.one111@gmail.com>
2023-07-16 22:12:25 +0300

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 
 import userRoutes from './routes/userRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
@@ -8,7 +9,7 @@ import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 6000;
 
 connectDB();
 
@@ -20,7 +21,15 @@ app.use(cookieParser());
 
 app.use('/api/users', userRoutes);
 
-app.get('/', (req, res) => res.send('server is ready'));
+if (process.env.NODE_ENV === 'production') {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, 'frontend', 'dist')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+  });
+} else {
+  app.get('/', (req, res) => res.send('server is ready'));
+}
 
 app.use(notFound);
 app.use(errorHandler);
